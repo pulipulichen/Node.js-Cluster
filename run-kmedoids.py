fa9674fca900f2fbca55de8ba1b259bbbd466188
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-12-16 20:31:10 +0800

    1216 太慢了啊！！

--- run-kmedoids.py
+++ run-kmedoids.py
@@ -9,6 +9,9 @@ import csv
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+cluster_number = config.getint('kmedoids', 'cluster_number')
+only_cluster_number = config.getboolean('kmedoids', 'only_cluster_number')
+
 # 3 points in dataset
 original_data = np.loadtxt('input/input.csv', delimiter=',', ndmin=2, dtype = np.float64)
 
@@ -30,7 +33,7 @@ for c_index in range(len(data[0])):
 # distance matrix
 D = pairwise_distances(data, metric='euclidean')
 
-cluster_number = config.getint('kmedoids', 'cluster_number')
+
 # split into 2 clusters
 M, C = kmedoids.kMedoids(D, cluster_number)
 
@@ -58,10 +61,13 @@ for i in range(len(data)):
                     row[r_index] = int(row[r_index])
             
             print('label {0} : {1}'.format(order_label[label], row))        
-                    
-            row.append(order_label[label])
-            #output_list.append(map(float, row))
-            output_list.append(row)
+            
+            if only_cluster_number == True:
+                output_list.append([order_label[label]])
+            else:
+                row.append(order_label[label])
+                #output_list.append(map(float, row))
+                output_list.append(row)
 
 print('')
 for i in range(len(order_label)):
@@ -69,7 +75,7 @@ for i in range(len(order_label)):
     print('label {0} count: {1}'.format(order_label[label], len(C[label])))
 
 # 輸出吧
-with open('output.csv', 'wb') as myfile:
+with open('output/output.csv', 'wb') as myfile:
     wr = csv.writer(myfile)
     for row in output_list:
         wr.writerow(row)

b7c560b40502064758451a5f78e50166dcb81e1f
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-12-29 13:50:36 +0800

    1229 好吧，把一些細節調整之後，分群功能就先做到這裡吧

--- python-lib/shenxudeu-K_Medoids/python-kmedoids.js
+++ python-lib/shenxudeu-K_Medoids/python-kmedoids.js
@@ -1,12 +1,66 @@
 PythonKMedoids = function (_matrix, _k, _max_iteration) {
     var _do_normalization = true;
-    
-    if (typeof(_max_iteration) === "undefined") {
-        _max_iteration = 50;
+
+    if (typeof (_max_iteration) === "undefined") {
+        _max_iteration = 150;
     }
-    
+
+
     //console.log(_matrix);
-    
+
+
+    // --------
+    // 處理缺漏值
+    var _col_median = {};
+    for (var _row_i in _matrix) {
+        for (var _col_i in _matrix[_row_i]) {
+            var _cell = _matrix[_row_i][_col_i];
+            if (isNaN(_cell) === true) {
+                continue;
+            }
+
+            if (typeof (_col_median[_col_i]) === "undefined") {
+                _col_median[_col_i] = [];
+            }
+            _col_median[_col_i].push(_cell);
+        }
+    }
+
+    // 轉換為中位數
+    var _calc_median = function (values) {
+
+        values.sort(function (a, b) {
+            return a - b;
+        });
+
+        var half = Math.floor(values.length / 2);
+
+        if (values.length % 2)
+            return values[half];
+        else
+            return (values[half - 1] + values[half]) / 2.0;
+    };
+
+    for (var _col_i in _col_median) {
+        _col_median[_col_i] = _calc_median(_col_median[_col_i]);
+    }
+
+    for (var _row_i in _matrix) {
+        for (var _col_i in _matrix[_row_i]) {
+            var _cell = _matrix[_row_i][_col_i];
+            
+            if (isNaN(_cell) === true || typeof(_cell) !== "number") {
+                
+                _matrix[_row_i][_col_i] = _col_median[_col_i];
+            }
+        }
+    }
+
+
+    //console.log(_matrix);
+
+
+
     // 正規化
     if (_do_normalization) {
         var _attr_max = {};
@@ -15,11 +69,10 @@ PythonKMedoids = function (_matrix, _k, _max_iteration) {
             for (var _col_i in _matrix[_row_i]) {
                 var _cell = _matrix[_row_i][_col_i];
 
-                if (typeof(_attr_max[_col_i]) === "undefined") {
+                if (typeof (_attr_max[_col_i]) === "undefined") {
                     _attr_max[_col_i] = _cell;
                     _attr_min[_col_i] = _cell;
-                }
-                else {
+                } else {
                     _attr_max[_col_i] = Math.max(_cell, _attr_max[_col_i]);
                     _attr_min[_col_i] = Math.min(_cell, _attr_min[_col_i]);
                 }
@@ -34,27 +87,27 @@ PythonKMedoids = function (_matrix, _k, _max_iteration) {
             }
         }
     }
-    
-    console.log(_matrix);
-    
+
+    //console.log(_matrix);
+
     // -----------------------
-    
+
     var _data = {
         matrix: _matrix,
         k: _k,
         max_iter: _max_iteration
     };
-    
+
     //console.log(JSON.stringify(_matrix));
-    
+
     // --------------------
-    
+
     var _result = false;
     var _done = false;
-    
+
     var _exec = function () {
 
-        var pyshell = new PythonShell('./python-lib/shenxudeu-K_Medoids/python-kmedoids.py', {mode:"json"});
+        var pyshell = new PythonShell('./python-lib/shenxudeu-K_Medoids/python-kmedoids.py', {mode: "json"});
 
         //console.log(_data);
         pyshell.send(_data);
@@ -66,27 +119,30 @@ PythonKMedoids = function (_matrix, _k, _max_iteration) {
 
         // end the input stream and allow the process to exit
         pyshell.end(function (err) {
-            if (err){
+            if (err) {
                 //throw err;
                 _result = err;
-            };
+            }
+            ;
 
             //console.log('finished');
             _done = true;
         });
     };
-    
+
     _exec();
-    
+
     var deasync = require('deasync');
-    deasync.loopWhile(function(){return !_done;});
+    deasync.loopWhile(function () {
+        return !_done;
+    });
 
     // ------------------------
     // 後置處理
-    
+
     //console.log(_result);
     //return _result;
-    
+
     var _array_avg = function (_array) {
         var _sum = 0;
         for (var _i in _array) {
@@ -94,19 +150,18 @@ PythonKMedoids = function (_matrix, _k, _max_iteration) {
         }
         return _sum / _array.length;
     };
-    
+
     var _uniq = function (a) {
         var b = JSON.parse(JSON.stringify(a));
         try {
-            return b.sort().filter(function(item, pos, ary) {
+            return b.sort().filter(function (item, pos, ary) {
                 return !pos || item !== ary[pos - 1];
             });
-        }
-        catch (e) {
+        } catch (e) {
             console.log(a);
         }
     };
-    
+
     // 先取出中心點列表
     var _result_medoids = _uniq(_result);
     var _medoids_list = [];
@@ -122,25 +177,29 @@ PythonKMedoids = function (_matrix, _k, _max_iteration) {
         });
         //console.log(_m_array);
     }
-    
-    var _sorted = _medoids_list.slice().sort(function(a,b){return a.avg-b.avg});
-    var _ranks = _medoids_list.slice().map(function(v){ return _sorted.indexOf(v)+1; });
-    
+
+    var _sorted = _medoids_list.slice().sort(function (a, b) {
+        return a.avg - b.avg
+    });
+    var _ranks = _medoids_list.slice().map(function (v) {
+        return _sorted.indexOf(v) + 1;
+    });
+
     var _medoids_dict = {};
     for (var _i = 0; _i < _medoids_list.length; _i++) {
         var _rank = _ranks[_i];
         var _m = _medoids_list[_i].m + "";
         _medoids_dict[_m] = _rank - 1;
     }
-    
+
     for (var _i in _result) {
-        
-        var _label = _medoids_dict[_result[_i]+""];
+
+        var _label = _medoids_dict[_result[_i] + ""];
         //console.log([_label, _result[_i]]);
         _result[_i] = _label;
     }
-    
-    console.log(_result);
+
+    //console.log(_result);
 
     return _result;
 };
